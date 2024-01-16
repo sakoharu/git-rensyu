cf9eee9e2b1cf9368c1194a308273fa47ca6f4bc
sakoharu <135291091+sakoharu@users.noreply.github.com>
2024-01-16 11:30:14 +0000

    model

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2024_01_13_173517) do
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
